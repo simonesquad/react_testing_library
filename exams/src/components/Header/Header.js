354522c8f97d12ab25ba001d1f40d299a8c968b8
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-24 15:11:39 -0500

    header tests passing

--- exams/src/components/Header/Header.js
+++ exams/src/components/Header/Header.js
@@ -6,8 +6,8 @@ export default function Header({
 }) {
     return (
         <>
-            <h1 title="Header" className="header">{title}</h1>
-            {/* <h3 data-testid="header-2" className="header">Hello</h3> */}
+            <h1 className="header" data-testid="header-1">{title}</h1>
+            <h3 title="Header" className="header">Cats</h3>
         </>
     )
 }
